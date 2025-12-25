edffb9e71387be7cf4e985c9a20baa9fe6a9ab6f
Leewonno <jsca5253@gmail.com>
2025-12-25 10:10:16 +0900

    feat : vite, typescript 버전 올리기, react-query devtool 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import ReactDOM from 'react-dom/client'
 import router from './Router'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
+    <TanStackDevtools plugins={[
+      {
+        name: 'TanStack Query',
+        render: <ReactQueryDevtoolsPanel />,
+        defaultOpen: true
+      },
+    ]} />
     <RouterProvider router={router}></RouterProvider>
   </QueryClientProvider>
 )
